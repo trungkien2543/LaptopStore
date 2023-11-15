c6d2ab3d52aa9f514d88899283624cea906b7fbf
ADMIN <ADMIN@26.217.121.86>
2023-11-15 18:20:02 +0700

    commit

--- src/main/java/DAO/TaiKhoan_DAO.java
+++ src/main/java/DAO/TaiKhoan_DAO.java
@@ -4,12 +4,16 @@
  */
 package DAO;
 
-import DTO.TaiKhoan;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+
+import DTO.*;
+
 /**
  *
  * @author DELL
@@ -18,35 +22,93 @@ public class TaiKhoan_DAO {
     Connection con;
 
     public TaiKhoan_DAO() {
-        try{
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=QLCuaHangLaptop;user=sa;password=1;"  + "encrypt=true;trustServerCertificate=true;sslProtocol=TLSv1.2;"); 
-            } 
-        catch(Exception e){
-            System.out.println(e); 
-        }       
+        con = new SQLConnection().getCon();
     }
-    
-    public ArrayList<TaiKhoan> getListTaiKhoan(){
-        ArrayList<TaiKhoan> list = new ArrayList<>();
-        String sql = "select * from TaiKhoan";
-        try{
-            PreparedStatement ps = con.prepareStatement(sql);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()){
-                TaiKhoan s = new TaiKhoan();
-                s.setTenDN(rs.getString(1));
-                s.setMatKhau(rs.getString(2));
-                s.setPhanQuyen(rs.getString(3));
-                list.add(s);
+    public List<TaiKhoan> getListTaiKhoan() {
+        List<TaiKhoan> taiKhoanList = new ArrayList<>();
+        String selectSQL = "SELECT * FROM TaiKhoan";
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement(selectSQL);
+			ResultSet rs = ps.executeQuery();
+	        
+	        // Lặp qua các hàng kết quả và thêm vào danh sách
+	        while (rs.next()) {
+	            String TenDN = rs.getString("TenDangNhap");
+	            String MatKhau = rs.getString("MatKhau");
+	            String PhanQuyen = rs.getString("PhanQuyen");
+	            TaiKhoan taiKhoan = new TaiKhoan(TenDN, MatKhau, PhanQuyen);
+	            taiKhoanList.add(taiKhoan);
+	        }
+	            
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        return taiKhoanList;
+	}
+    public NhanVien getNhanVienByTenDangNhap(String tenDangNhap) {
+        NhanVien nhanVien = null;
+        String sql = "SELECT nv.* FROM NhanVien nv INNER JOIN TaiKhoan tk ON nv.idNhanVien = tk.TenDangNhap WHERE tk.TenDangNhap = ?";
+
+        try (PreparedStatement preparedStatement = con.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, tenDangNhap);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    // Lấy thông tin từ ResultSet và tạo đối tượng NhanVien
+                    nhanVien = new NhanVien(
+                            resultSet.getString("idNhanVien"),
+                            resultSet.getString("TenNhanVien"),
+                            resultSet.getString("SDT"),
+                            resultSet.getString("DiaChi"),
+                            resultSet.getString("NgaySinh"),
+                            resultSet.getString("CCCD"),
+                            resultSet.getString("Email"),
+                            resultSet.getString("TrangThai"),
+                            resultSet.getBoolean("GioiTinh")
+                    );
+                }
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return list;
-    }   
-       
+        return nhanVien;
+    }
+
+    public NhanVien getNhanVienByEmail(String email) {
+        NhanVien nhanVien = null;
+        String sql = "SELECT * FROM NhanVien WHERE Email = ?";
+
+        try (PreparedStatement preparedStatement = con.prepareStatement(sql)) {
+        	
+            preparedStatement.setString(1, email);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    // Lấy thông tin từ ResultSet và tạo đối tượng NhanVien
+                    nhanVien = new NhanVien(
+                            resultSet.getString("idNhanVien"),
+                            resultSet.getString("TenNhanVien"),
+                            resultSet.getString("SDT"),
+                            resultSet.getString("DiaChi"),
+                            resultSet.getString("NgaySinh"),
+                            resultSet.getString("CCCD"),
+                            resultSet.getString("Email"),
+                            resultSet.getString("TrangThai"),
+                            resultSet.getBoolean("GioiTinh")
+                    );
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return nhanVien;
+    }
+    
+    
     public boolean addTaiKhoan(TaiKhoan tk){
         
         String sql = "insert into TaiKhoan (TenDangNhap, MatKhau, PhanQuyen) values (?,?,?)";
@@ -114,4 +176,8 @@ public class TaiKhoan_DAO {
         }
         return s;
     }
+    public static void main(String[] args) {
+		System.out.println(new TaiKhoan_DAO().getNhanVienByEmail("trankhanhduy12a12@gmail.com"));
+	}
+    // test github from eclipse
 }
